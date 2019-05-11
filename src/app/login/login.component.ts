5c4770fcdbe196eed1f0baef5b9a33f3672a20c1
Jozo Mihaljevic <jozo.mihaljevic.cox4@gmail.com>
2019-05-11 22:03:38 +0200

    ReactiveFormsModule added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Login } from '../shared/login.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  login: Login;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.getNew();
+  }
+
+  getNew() {
+    this.login = new Login();
+    this.setForm();
+  }
+
+  setForm() {
+    this.loginForm = this.fb.group({
+      email: new FormControl(this.login.email, Validators.required),
+      password: new FormControl(this.login.password, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
 
 }
