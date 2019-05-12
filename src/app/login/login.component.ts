2c0d6e021cfcfcfb75587554a2952c9fb5a1d56c
Jozo Mihaljevic <jozo.mihaljevic.cox4@gmail.com>
2019-05-12 18:28:19 +0200

    Email check, password strength check

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,13 @@ import { Login } from '../shared/login.model';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   login: Login;
+  notSubmitted = true;
+  eigthChar = false;
+  lowerCase = false;
+  upperCase = false;
+  numChar = false;
+  specialChar = false;
+  loginValue: Login;
 
   constructor(private fb: FormBuilder) { }
 
@@ -23,14 +30,48 @@ export class LoginComponent implements OnInit {
   }
 
   setForm() {
+    const regEmail = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+    const regPassword = '(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}';
     this.loginForm = this.fb.group({
-      email: new FormControl(this.login.email, Validators.required),
-      password: new FormControl(this.login.password, Validators.required)
+      email: new FormControl(this.login.email, [Validators.required, Validators.pattern(regEmail)]),
+      password: new FormControl(this.login.password, [Validators.required, Validators.pattern(regPassword)])
     });
+  }
+
+  onInput(event: any) {
+    const password = event.target.value;
+
+    if ((/[$@$!%*#?&]/.test(password))) {
+      this.specialChar = true;
+    } else {
+      this.specialChar = false;
+    }
+    if ((/\d/.test(password))) {
+      this.numChar = true;
+    } else {
+      this.numChar = false;
+    }
+    if ((/[a-z]/.test(password))) {
+      this.lowerCase = true;
+    } else {
+      this.lowerCase = false;
+    }
+    if ((/[A-Z]/.test(password))) {
+      this.upperCase = true;
+    } else {
+      this.upperCase = false;
+    }
+    if ((password.length >= 8)) {
+      this.eigthChar = true;
+    } else {
+      this.eigthChar = false;
+    }
   }
 
   onSubmit() {
     console.log(this.loginForm.value);
+    this.loginValue = this.loginForm.value;
+    this.notSubmitted = false;
   }
 
 }
